5f5ebaac6be4730fb49f70951f61ee7d4387b573
Peter Moser <p.moser@noi.bz.it>
2020-03-06 16:49:28 +0100

    CI: Do not start the flask server

--- Jenkinsfile-CI.groovy
+++ Jenkinsfile-CI.groovy
@@ -6,8 +6,9 @@ pipeline {
             steps {
 				sh '''
 					docker-compose --no-ansi build --pull --build-arg JENKINS_USER_ID=$(id -u jenkins) --build-arg JENKINS_GROUP_ID=$(id -g jenkins)
-					docker-compose --no-ansi run --rm --no-deps -u $(id -u jenkins):$(id -g jenkins) app
 				'''
+				// We do not have a proper test here, so keep this out for now
+				// docker-compose --no-ansi test --rm --no-deps -u $(id -u jenkins):$(id -g jenkins) app
             }
         }
     }
