13cf5475c71d7fa729b88c5297d13d9d62b28924
Peter Moser <p.moser@noi.bz.it>
2020-03-09 09:28:55 +0100

    Make update.sh verbose for debugging

--- update.sh
+++ update.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
+
+set -x
+
+echo "# STARTING update.sh"
 [ -f /tmp/update-running ] && return
 touch /tmp/update-running
 cd /code
 python download.py batch
 python to-numpy-data.py
 rm /tmp/update-running
+echo "# TERMINATING update.sh"
+
+exit 0
